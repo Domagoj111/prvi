16c3c7a598b7176ecf22caca8db489c3b97c7fa1
mrvic <mrvic.predrag@gmail.com>
2023-12-20 20:12:49 +0100

    php vars

---
+++ osnove_php/primjer_include.php
@@ -0,0 +1,5 @@
+<?php
+require '../osnove_html/forma-ispis.php';
+include '../osnove_html/forma-ispis.php';
+echo "neki text";
+?>
---
+++ osnove_php/varijable.php
@@ -0,0 +1,17 @@
+<?php
+
+$ime="ante";
+$Ime="Mate";
+
+echo $ime." ".$Ime;
+
+echo $jxfhjdhj986896="jhsdjhsa";
+
+define("PI",3.14);
+define($jxfhjdhj986896, $Ime);
+
+echo "<br>".PI;
+//echo "<br>".pi;  // konstante su case sensitive
+
+$radijus=5;
+echo "Opeg kruga je ".$radijus*2*PI;
