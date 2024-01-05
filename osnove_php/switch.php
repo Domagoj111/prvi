ace3b575f287e14c187008b024939e4331904078
mrvic <mrvic.predrag@gmail.com>
2024-01-05 21:08:38 +0100

    sitch

--- osnove_php/switch.php
+++ osnove_php/switch.php
@@ -44,3 +44,22 @@ switch (true) {
     echo "<hr>b NIJE izmedju a i c";
         break;
 }
+echo "<br>";
+date_default_timezone_set('Europe/Zagreb');
+$currentTime = date('Y-m-d H:i:s');
+echo "Local Time: $currentTime"; 
+echo "<br>l D d M Y H:i:s j N S w z W F m M n t L<br>";
+echo date('l D d M Y H:i:s j N S w z W F m M n t L',time());
+
+echo"<br>danas je ";
+
+
+switch(strtolower(date('D',time()))){
+ case "fri":
+ case "friday": echo "Petak"; break;
+ case "sun": 
+ case "sunday": echo "Nedjelja"; break;
+ case "mon": 
+ case "monday": echo "Ponedjeljak"; break;
+ default: echo "uto, sri, subota"; break;
+}
