4aaa57e2655d60eac149f5d4a18c92972607ec41
mrvic <mrvic.predrag@gmail.com>
2024-01-11 18:05:59 +0100

    setting.json za phph debug

--- osnove_php/funkcije_argumenti.php
+++ osnove_php/funkcije_argumenti.php
@@ -21,4 +21,4 @@ echo addNumbers(3, 4, true, 5);
 echo addNumbers(3, 4, true, 5, 2, 2, 43, 454, 5, 4, 67, 56, 7);
 echo addNumbers(3, 4, false, 5, 2, 2, 43, 454, 5, 4, 67, 56, 7);
 echo addNumbers(1,2,3,4,5,6,7,8,$printResult = true);
-echo addNumbers(1,2,3,4,5,6,7,8,$printResult = false);
+echo addNumbers(1,2,$printResult = false,3,4,5,6,7,8);
